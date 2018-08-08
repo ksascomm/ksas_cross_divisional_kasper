ef031d6277e9cc11f10b18d8b28c879300c4921e
ksascomm <ksasweb@jhu.edu>
2018-08-08 10:30:06 -0400

    .widget-sidebar class

--- front-page.php
+++ front-page.php
@@ -91,7 +91,7 @@
 
 			
 				<?php if ( is_active_sidebar('homepage0')  ) : ?>
-					<aside class="sidebar small-12 large-4 columns hide-for-print" id="sidebar1"> 
+					<aside class="sidebar widget-sidebar small-12 large-4 columns hide-for-print" id="sidebar1"> 
 						<?php dynamic_sidebar( 'homepage0' ); ?>
 					</aside>
 				<?php endif; ?>
